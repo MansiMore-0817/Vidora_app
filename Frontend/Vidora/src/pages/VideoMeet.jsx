8bbe98fcef3f9c4ce78d5ae5d742bd1e8775e084
Mansi More <moremansi0817@gmail.com>
2025-11-17 16:55:51 +0530

    Refactor video state management and implement WebSocket connection for real-time video conferencing

--- Frontend/Vidora/src/pages/VideoMeet.jsx
+++ Frontend/Vidora/src/pages/VideoMeet.jsx
@@ -22,7 +22,7 @@ function VideoMeetComp() {
 
   let [audioAvailable, setAudioAvailable] = useState(true);
 
-  let [video, setVideo] = useState();
+  let [video, setVideo] = useState([]);
   let [audio, setAudio] = useState();
 
   let [screen, setScreen] = useState();
@@ -135,10 +135,116 @@ function VideoMeetComp() {
     }
   }, [audio, video]);
 
+  // TODO
+  let gotMessageFromServer = (fromId, message) => {};
+
+  // TODO
+  let addMessage = () => {};
+
+  let connectToSocketServer = () => {
+    socketRef.current = new WebSocket(server_url, { secure: false });
+
+    socketRef.current.on("signal", gotMessageFromServer);
+
+    socketRef.current.on("connect", () => {
+      socketRef.current.emit("join-call", window.location.href);
+
+      socketIdRef.current = socketRef.current.id;
+
+      socketRef.current.on("chat-message", addMessage);
+
+      socketRef.current.on("user-left", (id) => {
+        setVideo((videos) => {
+          videos.filter((video) => video.socketId !== id);
+        });
+
+        socketRef.current.on("user-joined", (id, clients) => {
+          clients.forEach((socketListId) => {
+            connections[socketListId] = new RTCPeerConnection(
+              peerConfigConnections
+            );
+
+            connections[socketListId].onicecandidate = (event) => {
+              if (event.candidate != null) {
+                socketRef.current.emit(
+                  "signal",
+                  socketListId,
+                  JSON.stringify({ ice: event.candidate })
+                );
+              }
+            };
+
+            connections[socketListId].onaddstream = (event) => {
+              let videoExist = videoRef.current.find(
+                (video) => video.socketId === socketListId
+              );
+
+              if (videoExist) {
+                setVideo((videos) => {
+                  const updatedVideos = videos.map((video) => {
+                    video.socketId === socketListId
+                      ? { ...video, stream: event.stream }
+                      : video;
+                  });
+                  videoRef.current = updatedVideos;
+                  return updatedVideos;
+                });
+              } else {
+                let newVideo = {
+                  socketId: socketListId,
+                  stream: event.stream,
+                  autoPlay: true,
+                  playsInline: true,
+                };
+
+                setVideo((videos) => {
+                  const updatedVideos = [...videos, newVideo];
+                  videoRef.current = updatedVideos;
+                  return updatedVideos;
+                });
+              }
+            };
+
+            if (
+              window.localStream !== undefined &&
+              window.localStream !== null
+            ) {
+              connections[socketListId].addStream(window.localStream);
+            } else {
+              // TODO BlackSilence
+            }
+          });
+
+          if (id === socketIdRef.current) {
+            for (let id2 in connections) {
+              if (id2 === socketIdRef.current) continue;
+
+              try {
+                connections[id2].addStream(window.localStream);
+              } catch (e) {}
+              connections[id2].createOffer().then((description) => {
+                connections[id2].setLocalDescription(description).then(() => {
+                  socketRef.current
+                    .emit(
+                      "signal",
+                      id2,
+                      JSON.stringify({ sdp: connections[id2].localDescription })
+                    )
+                    .catch((e) => {
+                      console.log(e);
+                    });
+                });
+              });
+            }
+          }
+        });
+      });
+    });
+  };
+
   let getMedia = () => {
     setVideo(videoAvailable);
     setAudio(audioAvailable);
-    // connectToSocketServer();
   };
 
   return (
